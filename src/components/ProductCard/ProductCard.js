0c740603ef42e9faf1589bd7fa11b41a30bd3ec6
cnkilic <canklc9598@gmail.com>
2024-09-23 12:19:19 +0300

    add produc list component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -12,7 +12,7 @@ const ProductCard = ({
   const [imageHovered, setImageHovered] = useState(false);
 
   return (
-    <div className="h-full rounded-lg text-center shadow-lg flex flex-col min-w-[250px]">
+    <div className="h-full rounded-lg text-center shadow-lg flex flex-col min-w-[300px] bg-white">
       <img
         onMouseEnter={() => {
           setImageHovered(true);
@@ -28,7 +28,7 @@ const ProductCard = ({
         <div className="flex flex-col p-4 text-start">
           <h3 className="text-lg font-semibold mb-2">{title}</h3>
           {review && (
-            <div className="text-lg font- mb-2 flex gap-2">
+            <div className="text-sm mb-2 flex gap-2">
               <div>★★★★ </div>
               <div>{review.count}</div>
             </div>
@@ -36,7 +36,7 @@ const ProductCard = ({
           <p className="text-sm text-gray-700">€{price}</p>
         </div>
 
-        <div className="flex flex-col p-4">
+        <div className="flex flex-col p-4 pt-0">
           <button
             className="px-4 py-2 bg-black text-white rounded hover:bg-gray-700 transition duration-200 flex-grow"
             onClick={onButtonClick}
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,60 @@
+import React, { useRef } from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+const ProductList = ({ products }) => {
+  const scrollRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="max-w-full relative">
+      {/* Left Arrow */}
+      <button
+        className="absolute -left-5 top-1/2 transform -translate-y-1/2 bg-black text-white p-2 rounded-full  hover:bg-gray-700 z-10"
+        onClick={scrollLeft}
+      >
+        <FaArrowLeft />
+      </button>
+
+      {/* Scrollable Product List */}
+      {/* <div */}
+      <div
+        ref={scrollRef}
+        className="grid grid-flow-col-dense auto-cols-auto w-min max-w-full gap-6 overflow-x-auto pb-8 snap-mandatory snap-x scrollbar-none relative"
+      >
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            hoverImage={product.hoverImage}
+            title={product.title}
+            review={product.review}
+            price={product.price}
+            buttonText={product.buttonText}
+            onButtonClick={() => console.log(`${product.title} added to cart`)}
+          />
+        ))}
+      </div>
+
+      {/* Right Arrow */}
+      <button
+        className="absolute -right-5 top-1/2 transform -translate-y-1/2 bg-black text-white p-2 rounded-full z-10 hover:bg-gray-700"
+        onClick={scrollRight}
+      >
+        <FaArrowRight />
+      </button>
+    </div>
+  );
+};
+export default ProductList;
